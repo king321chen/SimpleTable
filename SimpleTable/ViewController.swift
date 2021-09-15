f9395a335f03f41518b423986faf5852f7a6f738
King Chen <48711761+king321chen@users.noreply.github.com>
2021-09-15 15:07:13 +0800

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by cobi321 on 2021/9/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
